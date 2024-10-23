80b97e8410af1aaac38161beee0914910f2d38d4
Hugo Dettner Källander <63727864+hugokallander@users.noreply.github.com>
2024-10-23 16:45:12 +0200

    Fix important message

--- index.js
+++ index.js
@@ -407,7 +407,7 @@ const clientReady = () => {
         channelIds.reminderOut = channelIds.test;
     }
 
-    await sendToChannelId(channelIds.announcementOut, "Daisy, Daisy, give me your answer do. I’m half crazy all for the love of you... It won’t be a stylish marriage, I can’t afford a carriage... But you’ll look sweet upon the seat of a bicycle built for... two...");
+    await sendToChannelId(channelIds.announcementOut, "Daisy, Daisy, give me your answer do. I’m half crazy all for the love of you... It won't be a stylish marriage, I can't afford a carriage... But you'll look sweet upon the seat of a bicycle built for... two...");
 
     console.log(`Logged in as ${client.user?.tag}!`);
 
